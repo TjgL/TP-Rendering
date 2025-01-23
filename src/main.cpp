b4e9e16915f3344ba4b80d22bf84ff5608073cff
TjgL <lithmoneo@gmail.com>
2025-01-23 14:35:10 +0100

    Created a basic triangle

--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,23 @@ int main()
 
     while (gl::window_is_open())
     {
+        glClearColor(0.f, 0.f, 1.f, 1.f);
+        glClear(GL_COLOR_BUFFER_BIT);
 
+        auto const triangle_mesh = gl::Mesh{
+                {
+                    .vertex_buffers = {
+                        {
+                            .layout = {gl::VertexAttribute::Position2D{0}},
+                            .data = {
+                                -1.f, -1.f,
+                                +1.f, -1.f,
+                                0.f, +1.f
+                            },
+                        }},
+                }};
+
+        gl::bind_default_shader();
+        triangle_mesh.draw();
     }
 }
